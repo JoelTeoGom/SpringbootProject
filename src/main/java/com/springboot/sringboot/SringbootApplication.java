5a15894bb90f5df7ce804df3f7a06cc33d584eb8
JoelTeoGom <joel.teodoro@estudiants.urv.cat>
2023-06-25 12:04:55 +0200

    first commit

---
+++ src/main/java/com/springboot/sringboot/SringbootApplication.java
@@ -0,0 +1,25 @@
+package com.springboot.sringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class SringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringbootApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public List<String> hello() {
+		return List.of("Hello", "World");
+	}
+
+
+
+}
---
+++ src/test/java/com/springboot/sringboot/SringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.sringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
